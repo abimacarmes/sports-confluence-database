d43683a095723746284b05883424f186bef2ebff
Abigail M-A <a.mackenzie.armes@gmail.com>
2021-07-07 23:07:27 -0400

    updated testing

--- test/app.test.js
+++ test/app.test.js
@@ -2,16 +2,14 @@ const { expect } = require('chai');
 const knex = require('knex');
 const supertest = require('supertest');
 const app = require('../src/app');
-const databaseService = require('../src/databaseService')
-const STORE = require('../dummy-store')
 
-describe('Spaces Endpoints', () => {
+describe('Games & Players Endpoints', () => {
     let db;
 
     before("make knex instance", () => {
         db = knex({
         client: "pg",
-        connection: process.env.TEST_DATABASE_URL,
+        connection: 'postgresql://postgres@localhost/sports-confluence-database-test',
         });
         app.set("db", db);
     });
@@ -21,12 +19,12 @@ describe('Spaces Endpoints', () => {
     it('should return a message from GET /api/', () => {
         return supertest(app)
             .get('/api/')
-            .expect(200, 'Database Endpoint Homepage')
+            .expect(200, 'Sports Confluence App - Database Endpoint Homepage')
     });
 
-    it('GET /api/spaces - given that there is at least one space', () => {
+    it('GET /api/games - given that there is at least one game', () => {
         return supertest(app)
-            .get('/api/spaces')
+            .get('/api/games')
             .expect(200)
             .expect('Content-Type', /json/)
             .then(res => {
@@ -34,36 +32,53 @@ describe('Spaces Endpoints', () => {
                 expect(res.body).to.have.lengthOf.at.least(1);
                 const result = res.body;
                 expect(result[0]).to.include.all.keys(
-                    'id','name','address','city','type','upcount','downcount'
+                    'game_id','name','sport','location_name','address','players','date'
                 );
             });
     });
 
-    it('POST /api/spaces', () => {
-        const newSpace = {
-            "name": "test location",
-            "address":"123 place lane",
-            "city":"Toronto",
-            "type": "Restaurant"
+    it('GET /api/players - given that there is at least one player', () => {
+        return supertest(app)
+            .get('/api/players')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => {
+                expect(res.body).to.be.an('array');
+                expect(res.body).to.have.lengthOf.at.least(1);
+                const result = res.body;
+                expect(result[0]).to.include.all.keys(
+                    'player_id','name','level','comment','game_id'
+                );
+            });
+    });
+
+    it('POST /api/games', () => {
+        const newGame = {
+            "name":"crush em football",
+            "sport": "Football",
+            "location_name":"Park Park",
+            "address":"123 place lane, Vancouver",
+            "players": 12,
+            "date":"08/08/2021"
         }
 
         return supertest(app)
-            .post('/api/spaces')
-            .send(newSpace)
-            .expect(201,newSpace)
-    })
+            .post('/api/games')
+            .send(newGame)
+            .expect(201,newGame)
+    });
 
-    it('PATCH /api/spaces/:id - given the id exists', () => {
-        const updatedInfo = {
-            "id": 1,
-            "upcount":2,
-            "downcount":2
+    it('POST /api/players', () => {
+        const newPlayer = {
+            "name":"test",
+            "level":"Beginner",
+            "comment":"Test comment",
+            "game_id":1
         }
 
         return supertest(app)
-            .patch(`/api/spaces/${updatedInfo.id}`)
-            .send(updatedInfo)
-            .expect(201,updatedInfo)
-
+            .post('/api/players')
+            .send(newPlayer)
+            .expect(201, newPlayer)
     })
 });
